3abe1f0e52dda84ec43c45ab47dc9208f995046b
lucas <lucasspiteri@protonmail.com>
2025-11-11 16:18:55 +0100

    Refactor compile_exe to accept dependencies and improve file tracking

--- nob.c
+++ nob.c
@@ -18,10 +18,15 @@
 
 Nob_Procs procs = {0};
 
-static int compile_exe(const char **source_files, size_t src_count, const char *output_file)
+static int compile_exe(
+    const char **source_files, size_t src_count, //
+    const char **deps, size_t deps_count,        //
+    const char *output_file                      //
+)
 {
     int ret = 0;
     Nob_Cmd cmd = {0};
+    Nob_File_Paths dep_files = {0};
 
 #ifdef _WIN32
     nob_cmd_append(&cmd, CC);
@@ -38,11 +43,17 @@ static int compile_exe(const char **source_files, size_t src_count, const char *
     for (size_t i = 0; i < src_count; i++)
     {
         nob_cc_inputs(&cmd, source_files[i]);
+        nob_da_append(&dep_files, source_files[i]);
+    }
+
+    for (size_t i = 0; i < deps_count; i++)
+    {
+        nob_da_append(&dep_files, deps[i]);
     }
 
     nob_cmd_append(&cmd, LINK_FLAGS);
 
-    if (!nob_needs_rebuild(output_file, source_files, src_count))
+    if (!nob_needs_rebuild(output_file, dep_files.items, dep_files.count))
     {
         nob_log(NOB_INFO, "%s up to date", output_file);
         ret = 0;
@@ -58,6 +69,7 @@ static int compile_exe(const char **source_files, size_t src_count, const char *
 
 defer:
     nob_cmd_free(cmd);
+    nob_da_free(dep_files);
     return 0;
 }
 
@@ -89,7 +101,11 @@ static int compile_tests(const char **tests, size_t test_count)
 #endif
         nob_sb_append_null(&sb);
 
-        if (0 != compile_exe(&test_source, 1, sb.items))
+        const char **deps = (const char *[]){
+            "stb_teapot.h",
+        };
+
+        if (0 != compile_exe(&test_source, 1, deps, NOB_ARRAY_LEN(deps), sb.items))
         {
             ret = 1;
             goto defer;
